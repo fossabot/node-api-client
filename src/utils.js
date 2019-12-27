4422b30a59192d832dd1c2b71102f21631f69117
Bang Duong <52025132+bangretraced@users.noreply.github.com>
2019-12-27 10:09:25 +0000

    feat: AJV validate clone data instead of original data (#18)

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,13 @@
 const Ajv = require('ajv')
+const R = require('ramda')
 
 const ajv = new Ajv({removeAdditional: true, allErrors: true})
 
 module.exports.ajvValidate = (schema, data) => {
 	if (!schema) return data
-	const valid = ajv.validate(schema, data)
+	// It will not work with Object.assign or spread copy {...data}
+	const cloneData = R.clone(data)
+	const valid = ajv.validate(schema, cloneData)
 	if (!valid) throw ajv.errors
-	return data
+	return cloneData
 }
---
+++ test/ajv-validate.test.js
@@ -0,0 +1,65 @@
+const schemas = require('@retracedgmbh/schemas')
+const utils = require('../src/utils')
+
+describe('AJV validation test', () => {
+	describe('Validate data schema with additional properties', () => {
+		test('Validate simple object', async () => {
+			const data = {id: 'test-id', url: 'file://url'}
+
+			const returnedData = utils.ajvValidate(schemas.request.picture.single, data)
+
+			expect(returnedData.id).toEqual('test-id')
+			expect(returnedData.url).toEqual(undefined)
+			expect(data.url).toEqual('file://url')
+		})
+
+		test('Validate array object', async () => {
+			const data = [
+				{
+					id: 'id1',
+					url: 'file://id1'
+				},
+				{
+					id: 'id2',
+					url: 'file://id2'
+				}
+			]
+
+			const returnedData = utils.ajvValidate(schemas.request.picture.list, data)
+			expect(returnedData[0].id).toEqual('id1')
+			expect(returnedData[0].url).toEqual(undefined)
+			expect(data[0].url).toEqual('file://id1')
+		})
+
+		test('Validate nested object', async () => {
+			const validateSchema = {
+				type: 'object',
+				required: ['name', 'pictures'],
+				additionalProperties: false,
+				properties: {
+					name: {type: 'string', minLength: 1},
+					pictures: schemas.request.picture.list
+				}
+			}
+
+			const data = {
+				name: 'Name',
+				pictures: [
+					{
+						id: 'id1',
+						url: 'file://id1'
+					}
+				],
+				description: 'D'
+			}
+
+			const returnedData = utils.ajvValidate(validateSchema, data)
+
+			expect(returnedData.name).toEqual('Name')
+			expect(returnedData.description).toEqual(undefined)
+			expect(returnedData.pictures[0].id).toEqual('id1')
+			expect(returnedData.pictures[0].url).toEqual(undefined)
+			expect(data.pictures[0].url).toEqual('file://id1')
+		})
+	})
+})
