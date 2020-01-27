d232bb4940668eb9620ad183ebdb46caf6138c07
Nefeli Lygourioti <nef@retraced.co>
2020-01-27 16:42:57 +0200

    fix: Typo on requestJSON (#47)

--- src/endpoints/analytics.js
+++ src/endpoints/analytics.js
@@ -1,13 +1,8 @@
-const {stringify} = require('query-string')
 const Endpoint = require('../endpoint')
 
 class Analytics extends Endpoint {
 	get(accessToken, {type, start, end}) {
-		return this.requestJSON(
-			accessToken,
-			'GET',
-			`analytics/analysis/?type=${type}&start=${stringify({start})}&end=${stringify({end})}`
-		)
+		return this.requestJSON(accessToken, 'GET', `analytics/analysis?type=${type}&start=${start}&end=${end}`)
 	}
 }
 
